4a4605f74326466ab571510e4188455480ef3d13
Leo McClellan <mcclellan2@usf.edu>
2024-09-17 10:23:20 -0400

    Initialize inventory with product objects

---
+++ order_man.js
@@ -0,0 +1,9 @@
+// Coding Challenge 5: Customer Order Management System
+
+// Task 1: Create an inventory array of product objects
+let inventory = [
+    {"name": "latte", "price": 4, "quantity": 25},
+    {"name": "mocha", "price": 5, "quantity": 15},
+    {"name": "espresso", "price": 3, "quantity": 30},
+    {"name": "frappuccino", "price": 7, "quantity": 20}
+]
